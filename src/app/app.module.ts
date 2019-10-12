e5c4e853108915a32c6921c232208694552ff2b6
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2019-10-12 15:41:09 +0100

    added a test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponentComponent } from './profile-component/profile-component.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponentComponent
+    ProfileComponentComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
